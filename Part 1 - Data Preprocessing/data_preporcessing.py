bc861b0c2e275fdf28183040225f8e194f51409d
hoinarut <tudor.hoinaru@gmail.com>
2017-08-03 09:31:10 +0300

    Missing data

--- Part 1 - Data Preprocessing/data_preporcessing.py
+++ Part 1 - Data Preprocessing/data_preporcessing.py
@@ -1,7 +1,15 @@
+#Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#Import dataset
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+#Handle missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN',strategy='mean',axis=0)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform()
